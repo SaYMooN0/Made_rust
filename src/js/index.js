b8c6b48e2fd66db6edd8e7d199b635576c63304d
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-25 21:27:00 +0300

    Keep on working on the main page

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,9 @@ const { invoke } = window.__TAURI__.tauri;
 let greetInputEl;
 let greetMsgEl;
 
+function clicked() {
+    alert("click");
+}
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
