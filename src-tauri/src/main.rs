2018e10161da55cc8fca7c92923993c063c4e884
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-25 11:43:15 +0300

    basic functionality of changing the theme is ready

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,26 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_color() -> String {
+    let color: String="#bdda57".to_string();
+    return  color;
 }
+
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    
+    let string:String= format!("New, {}! You've been greeted from Rust!", name);
+    return  string;
+}
+
+
+
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_color])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,20 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
-
+async function divClicked()
+{
+  let divElement=document.getElementById("myDivId");
+  let newText = await invoke("get_color");
+  divElement.style.color=newText;
+  divElement.textContent=newText;
+}
+function switchTheme(newTheme) {
+  var linkElement = document.querySelector('#theme-link');
+  linkElement.href = 'themes_css/' + newTheme;
+}
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
+  // greetInputEl = document.querySelector("#greet-input");
+  // greetMsgEl = document.querySelector("#greet-msg");
+  // document.querySelector("#greet-button").addEventListener("click", () => divClicked());
+  document.getElementById("div1").addEventListener("click", () => switchTheme("default_light.css"));
 });
