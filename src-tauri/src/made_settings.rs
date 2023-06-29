2eb0740e5415b23ce722f21e65b43d1a784902d1
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-29 22:20:50 +0300

    Added rust code for uploading and adding projects

--- src-tauri/src/made_settings.rs
+++ src-tauri/src/made_settings.rs
@@ -1,47 +1,100 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
-use std::str::FromStr;
-use std::path::Path;
-use std::fmt;
 use std::collections::HashMap;
+use std::fmt;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write, Seek, SeekFrom};
+use std::path::Path;
+use std::str::FromStr;
+
 pub struct made_settings {
     current_theme: Theme,
     projects: HashMap<String, String>,
 }
+
 impl made_settings {
     pub fn new() -> Self {
-        let file_path = Path::new("themes.madeSettings");
+        let theme_file_path = Path::new("themes.madeSettings");
+        let links_file_path = Path::new("links.madeSettings");
+
         let default_theme_str = "dark";
         let default_theme = Theme::Dark;
 
-        let file_content = if !file_path.exists() {
-            let mut file = File::create(&file_path).expect("Failed to create file");
-            write!(file, "{}", default_theme_str).expect("Failed to write to file");
-            default_theme_str.to_string()
-        } else {
-            let mut file = OpenOptions::new()
-                .read(true)
-                .open(file_path)
-                .expect("Failed to open file");
-            let mut content = String::new();
-            file.read_to_string(&mut content).expect("Failed to read from file");
-            content
-        };
+        let theme_file_content = Self::read_or_create_file(&theme_file_path, &default_theme_str);
 
-        let current_theme = Theme::from_str(file_content.trim()).unwrap_or_else(|_| {
-            let mut file = OpenOptions::new()
-                .write(true)
-                .open(file_path)
-                .expect("Failed to open file");
-            write!(file, "{}", default_theme_str).expect("Failed to write to file");
+        let current_theme = Theme::from_str(theme_file_content.trim()).unwrap_or_else(|_| {
+            Self::write_to_file(&theme_file_path, &default_theme_str);
             default_theme
         });
 
-        made_settings { current_theme, projects: HashMap::new() }
+        let mut projects = HashMap::new();
+
+        Self::read_or_create_file(&links_file_path, "");
+
+        let links_file_content = Self::read_from_file(&links_file_path);
+        for line in links_file_content.lines() {
+            let parts: Vec<&str> = line.splitn(2, '=').collect();
+            if parts.len() == 2 {
+                projects.insert(parts[0].to_string(), parts[1].to_string());
+            }
+        }
+
+        made_settings {
+            current_theme,
+            projects,
+        }
+    }
+
+    fn read_or_create_file(file_path: &Path, default_content: &str) -> String {
+        if !file_path.exists() {
+            let mut file = File::create(file_path).expect("Failed to create file");
+            write!(file, "{}", default_content).expect("Failed to write to file");
+            default_content.to_string()
+        } else {
+            Self::read_from_file(file_path)
+        }
+    }
+
+    fn read_from_file(file_path: &Path) -> String {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(file_path)
+            .expect("Failed to open file");
+        let mut content = String::new();
+        file.read_to_string(&mut content)
+            .expect("Failed to read from file");
+        content
+    }
+
+    fn write_to_file(file_path: &Path, content: &str) {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .open(file_path)
+            .expect("Failed to open file");
+        write!(file, "{}", content).expect("Failed to write to file");
     }
 
     pub fn get_current_theme(&self) -> String {
         self.current_theme.to_string()
+    }
+    pub fn get_projects_dictionary(&self) -> HashMap<String, String> {
+        self.projects.clone()
+    }
+    pub fn add_project(&mut self, name: String, path: String) {
+        // Add to the HashMap
+        self.projects.insert(name.clone(), path.clone());
+
+        // Now write to the file
+        let links_file_path = Path::new("links.madeSettings");
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(&links_file_path)
+            .expect("Failed to open file");
+
+        // Always add a new line before the project
+        writeln!(file).expect("Failed to write new line to file");
+
+        // Now write the new project
+        writeln!(file, "{}={}", name, path).expect("Failed to write to file");
     }
 }
 enum Theme {
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::sync::Mutex;
+use std::{sync::Mutex, collections::HashMap};
 use lazy_static::lazy_static;
 mod made_settings;
 
@@ -8,15 +8,29 @@ mod made_settings;
 lazy_static! {
     static ref SETTINGS: Mutex<made_settings::made_settings> = Mutex::new(made_settings::made_settings::new());
 }
-
+#[tauri::command]
+fn get_projects() -> HashMap<String, String> {
+    let mut setting_for_made = SETTINGS.lock().unwrap();
+    return setting_for_made.get_projects_dictionary();
+}
 #[tauri::command]
 fn get_current_theme_name() -> String {
     let setting_for_made = SETTINGS.lock().unwrap();
     return setting_for_made.get_current_theme();
 }
+#[tauri::command]
+fn add_project(name:String,path:String){
+    let mut setting_for_made = SETTINGS.lock().unwrap();
+    setting_for_made.add_project(name, path);
+}
 fn main() {
+    let book_reviews = get_projects();
+    add_project("name3".to_string(), "path2".to_string());
+    
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_current_theme_name])
+        .invoke_handler(tauri::generate_handler![get_projects])
+        .invoke_handler(tauri::generate_handler![add_project])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
