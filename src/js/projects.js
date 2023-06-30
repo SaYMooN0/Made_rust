ff68a9bc45d61510482034ab49a01de60d9afde7
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-30 20:43:32 +0300

    keep on working on the projects page

--- src/js/projects.js
+++ src/js/projects.js
@@ -6,17 +6,33 @@ async function setThemeOnStart() {
     let theme = await invoke("get_current_theme_name");
     linkElement.href = '../themes_css/' + theme + ".css";
 }
-
+function projectLinkClicked()
+{
+    pr.textContent="clicked";
+    pr.textContent = this.textContent;
+}
 async function setProjects() {
     
     projectsContainer = document.getElementById('projects-container');
     let projects = await invoke("get_projects");
     let projectItem={};
+    let projectTextName={}, projectTextPath={};
     for (const [key, value] of Object.entries(projects)) {
         projectItem = document.createElement('div');
         projectItem.className = "project-item";
-        projectItem.id = key;
-        projectItem.textContent = key+": " + value;
+        projectItem.id = "prj_item_"+key;
+        projectItem.addEventListener('click', projectLinkClicked);
+      
+        projectTextName=document.createElement('p');
+        projectTextName.textContent = key;
+        projectTextName.className = "project-text-name";
+        projectItem.appendChild(projectTextName);
+
+        projectTextPath=document.createElement('p');
+        projectTextPath.textContent = value;
+        projectTextPath.className = "project-text-path";
+        projectItem.appendChild(projectTextPath);
+
         projectsContainer.appendChild(projectItem);
     }
     pr.textContent = "worked";
