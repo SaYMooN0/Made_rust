bbc6bc9c4ed16aaed2177eb21f06fb21955eb3dd
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-04 04:06:05 +0300

    The work on the dialog box styles is finished. most likely this is not the final version

--- src/js/projects.js
+++ src/js/projects.js
@@ -25,8 +25,8 @@ function createCorruptedSign(name) {
     dialog.innerHTML = `
         <div class="corrupted-sign-dialog-container">
         <p class="csd-txt">The Made file "${name}" is corrupted and cannot be opened. File recovery is very unlikely. In this situation, it is recommended to delete it and recreate it. Deleting the Made file will not entail any changes in your modpack. Only data about collections of items and tags inside Made will be lost. Would you like to delete a corrupted file:</p>
-        <button class="csd-btn csd-btn-cncl" id="cancel">Cancel</button>
-        <button class="csd-btn-dlt" id="delete">Delete</button>
+        <div class="csd-btn csd-btn-cncl" id="cancel">Cancel</div>
+        <div class="csd-btn csd-btn-dlt" id="delete">Delete</div>
         </div>
     `;
     dialog.querySelector("#cancel").addEventListener('click', function() {
