5ddc7515d5507257b36cada19d7363b6c70ed81a
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-05 16:16:26 +0300

    started to work on the "create new project" action dialog window

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,6 +1,7 @@
 const { invoke } = window.__TAURI__.tauri;
 var projectsContainer = {};
 var pr = {};
+
 async function setThemeOnStart() {
     var linkElement = document.getElementById('theme-link');
     let theme = await invoke("get_current_theme_name");
@@ -14,10 +15,10 @@ function createCorruptedSign(name) {
     let sign = document.createElement('div');
     sign.className = "corrupted-sign";
     sign.innerHTML = '<?xml version="1.0" encoding="utf-8"?><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z" stroke="#7E032A" class="corrupted-sign-line" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
-    sign.addEventListener('mouseover', function() {
+    sign.addEventListener('mouseover', function () {
         this.parentNode.style.backgroundColor = 'var(--main-backcolor)';
     });
-    sign.addEventListener('mouseout', function() {
+    sign.addEventListener('mouseout', function () {
         this.parentNode.style.backgroundColor = '';
     });
     let dialog = document.createElement('dialog');
@@ -29,11 +30,11 @@ function createCorruptedSign(name) {
         <div class="csd-btn csd-btn-dlt" id="delete">Delete</div>
         </div>
     `;
-    dialog.querySelector("#cancel").addEventListener('click', function() {
+    dialog.querySelector("#cancel").addEventListener('click', function () {
         dialog.close();
     });
-    dialog.querySelector("#delete").addEventListener('click', function() {
-        
+    dialog.querySelector("#delete").addEventListener('click', function () {
+
         let projectToDelete = document.getElementById("prj_item_" + name);
         projectToDelete.remove();
         dialog.close();
@@ -41,7 +42,7 @@ function createCorruptedSign(name) {
     });
 
     document.body.appendChild(dialog);
-    sign.addEventListener('click', function() {
+    sign.addEventListener('click', function () {
         dialog.showModal();
     });
 
@@ -68,10 +69,10 @@ function generateProjectLink(projectInfo) {
     projectMoreButton = document.createElement('svg');
     projectMoreButton.innerHTML = '<?xml version="1.0" encoding="iso-8859-1"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 342.382 342.382"xml:space="preserve"><g> <g> <g><path d="M45.225,125.972C20.284,125.972,0,146.256,0,171.191c0,24.94,20.284,45.219,45.225,45.219c24.926,0,45.219-20.278,45.219-45.219C90.444,146.256,70.151,125.972,45.225,125.972z"/></g><g> <path d="M173.409,125.972c-24.938,0-45.225,20.284-45.225,45.219c0,24.94,20.287,45.219,45.225,45.219c24.936,0,45.226-20.278,45.226-45.219C218.635,146.256,198.345,125.972,173.409,125.972z"/></g><g><path d="M297.165,125.972c-24.932,0-45.222,20.284-45.222,45.219c0,24.94,20.29,45.219,45.222,45.219 c24.926,0,45.217-20.278,45.217-45.219C342.382,146.256,322.091,125.972,297.165,125.972z"/></g>  </g></g> </svg>';
     projectMoreButton.className = "project-more-button";
-    projectMoreButton.addEventListener('mouseover', function() {
+    projectMoreButton.addEventListener('mouseover', function () {
         this.parentNode.style.backgroundColor = 'var(--main-backcolor)';
     });
-    projectMoreButton.addEventListener('mouseout', function() {
+    projectMoreButton.addEventListener('mouseout', function () {
         this.parentNode.style.backgroundColor = '';
     });
     projectItem.appendChild(projectMoreButton);
@@ -103,8 +104,13 @@ async function setProjects() {
 
     }
 }
-
+function addEvents()
+{
+    document.getElementById("openCreateProjectDialogBtn").addEventListener("click",function() {document.getElementById('my-dialog').showModal();});
+    document.getElementById("dialogCancelBtn").addEventListener("click",function() {document.getElementById('my-dialog').close();});
+}
 window.addEventListener("DOMContentLoaded", async () => {
+    addEvents();
     pr = document.getElementById('project-header');
     await setProjects();
     await setThemeOnStart();
