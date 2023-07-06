0492e3473bde6a809ea5c62d931e451a255af24a
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-06 23:57:20 +0300

    Now it is clear what the inputs in the form are responsible for. Added a working automatic substitution of the values of the version and the name of the Made project in the inputs

--- src/js/projects.js
+++ src/js/projects.js
@@ -109,13 +109,18 @@ function addEvents() {
     document.getElementById("openCreateProjectDialogBtn").addEventListener("click", function () { document.getElementById('createNewProjectDialog').showModal(); });
     document.getElementById("dialogCancelBtn").addEventListener("click", function () { document.getElementById('createNewProjectDialog').close(); });
     let result;
-    document.getElementById("openFileExplorerBtn").addEventListener("click", async function () { 
-        let directory=await open({ multiple: false, directory: true, }); 
-        console.log(directory);
+    document.getElementById("openFileExplorerBtn").addEventListener("click", async function () {
+        let directory = await open({ multiple: false, directory: true, });
         document.getElementById("path-input").value = directory;
-        let nameAndVersionObject=await invoke("get_name_and_version", { directoryName: directory });
-        console.log(nameAndVersionObject);
-        document.getElementById("name-input").value="1" ;
+        let nameAndVersionObject = await invoke("get_name_and_version", { directoryName: directory });
+        if (nameAndVersionObject != "-1") {
+            document.getElementById("name-input").value = nameAndVersionObject.toString().split('|')[1];
+            document.getElementById("version-input").value = nameAndVersionObject.toString().split('|')[0];
+        }
+        else
+        {
+            
+        }
     });
     console.log(result);
 }
