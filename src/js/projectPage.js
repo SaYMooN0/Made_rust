310841acdade4a7d6d8e2e99797fbb1c27427c20
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-09 22:25:35 +0300

    Added jquery to the files. Started to form projectPage html

--- src/js/projectPage.js
+++ src/js/projectPage.js
@@ -1,6 +1,22 @@
 const { invoke } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog;
 var project;
+const tabs = document.querySelectorAll('.tab');
+$( function() {
+    $("#tabs").tabs();
+    $("#sortable").sortable({
+      axis: "x",
+      update: function(event, ui) {
+        var sortedIDs = $("#sortable").sortable("toArray");
+        console.log(sortedIDs);
+      }
+    });
+  
+    $("#sortable").disableSelection();
+  } );
+function main() {
+
+}
 async function setThemeOnStart() {
     var linkElement = document.getElementById('theme-link');
     let theme = await invoke("get_current_theme_name");
@@ -8,6 +24,6 @@ async function setThemeOnStart() {
 }
 window.addEventListener("DOMContentLoaded", async () => {
     await setThemeOnStart();
-    project= await invoke("get_current_project");
-    console.log(project);
+    project = await invoke("get_current_project");
+    main();
 });
