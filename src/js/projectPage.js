0fd47f31e9fd663bb1ccea40c826e02156419c0b
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-10 00:40:59 +0300

    started working on the project page, forming a separator and tabs

--- src/js/projectPage.js
+++ src/js/projectPage.js
@@ -1,21 +1,53 @@
+import tabM from './projectPageFiles/pages/tab.js';
 const { invoke } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog;
-var project;
-const tabs = document.querySelectorAll('.tab');
-$( function() {
-    $("#tabs").tabs();
-    $("#sortable").sortable({
-      axis: "x",
-      update: function(event, ui) {
-        var sortedIDs = $("#sortable").sortable("toArray");
-        console.log(sortedIDs);
-      }
-    });
-  
-    $("#sortable").disableSelection();
-  } );
-function main() {
+var project, leftDiv, rightDiv;
+function addTab(tabName) {
+    let newTab = $("<li></li>");
+    let newLink = $("<a></a>").attr("href", "#" + tabName).text(tabName);
+    newTab.append(newLink);
+    $("#sortable").append(newTab);
+    $("#containerForPages").tabs("refresh");
+    $("#sortable").sortable("refresh");
+}
 
+$(function () {
+    $("#containerForPages").tabs();
+    $("#sortable").sortable({
+        axis: "x"
+    });
+    $("#sortable").disableSelection();
+});
+
+function main() {
+    // document.getElementById('tab-1').innerHTML += tabM;
+    let divider = document.getElementById('divider');
+    leftDiv = document.getElementById('div1');
+    rightDiv = document.getElementById('div2');
+    divider.addEventListener('mousedown', mouseDown);
+}
+function mouseDown(e) {
+    e.preventDefault();
+
+    let containerWidth = document.getElementById('mainContainer').offsetWidth;
+
+    window.addEventListener('mousemove', mouseMove);
+    window.addEventListener('mouseup', mouseUp);
+
+    function mouseMove(e) {
+        let newWidth = (e.clientX / containerWidth) * 100;
+
+        // Максимальная и минимальная ширина div'ов
+        if (newWidth > 20 && newWidth < 60) {
+            leftDiv.style.width = newWidth + '%';
+            rightDiv.style.width = (100 - newWidth) + '%';
+        }
+    }
+
+    function mouseUp() {
+        window.removeEventListener('mousemove', mouseMove);
+        window.removeEventListener('mouseup', mouseUp);
+    }
 }
 async function setThemeOnStart() {
     var linkElement = document.getElementById('theme-link');
---
+++ src/js/projectPageFiles/pages/tab.js
@@ -0,0 +1,6 @@
+const tabM = `
+<header>
+  <h1>Это мой хедер</h1>
+</header>
+`;
+export default tabM;
