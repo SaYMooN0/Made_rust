d66d97669ee0dbf4c240bafa0d340eb6babc224c
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-11 00:07:08 +0300

    abandoned the idea of resizable parts for the choice of modification and the modification itself

--- src/js/projectPage.js
+++ src/js/projectPage.js
@@ -1,16 +1,9 @@
 import tabM from './projectPageFiles/pages/tab.js';
 const { invoke } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog;
-var project, choosingDiv, makingDiv;
+var project;
 function main() {
-    
     // document.getElementById('tab-1').innerHTML += tabM;
-    let divider = document.getElementById('divider');
-    choosingDiv = document.getElementById('modificationsChoosingPart');
-    makingDiv = document.getElementById('modificationsMakingPart');
-    divider.addEventListener('mousedown', mouseDown);
-    choosingDiv.style.width = '20%';
-    makingDiv.style.width = '80%';
 }
 function addTab(tabName) {
     let newTab = $("<li></li>");
@@ -27,25 +20,6 @@ $(function () {
     });
     $("#sortable").disableSelection();
 });
-function mouseDown(e) {
-    e.preventDefault();
-    let containerWidth = document.getElementById('mainContainer').offsetWidth;
-    window.addEventListener('mousemove', mouseMove);
-    window.addEventListener('mouseup', mouseUp);
-    function mouseMove(e) {
-        let newWidth = (e.clientX / containerWidth) * 100;
-        if (newWidth > 14 && newWidth < 40) {
-            choosingDiv.style.width = newWidth + '%';
-            makingDiv.style.width = (100 - newWidth) + '%';
-            console.log(choosingDiv);
-        }
-    }
-
-    function mouseUp() {
-        window.removeEventListener('mousemove', mouseMove);
-        window.removeEventListener('mouseup', mouseUp);
-    }
-}
 async function setThemeOnStart() {
     var linkElement = document.getElementById('theme-link');
     let theme = await invoke("get_current_theme_name");
