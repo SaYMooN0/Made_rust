3db98bb8d605aca076c51b07f3969eb7961a8210
SaYMooN <shokoladnysaymoon@mail.ru>
2023-07-13 13:46:11 +0300

    started working on checking the project version when creating it. In the absence of kubjs for this version, the creation of the project will be prohibited

--- src/js/createNewProject.js
+++ src/js/createNewProject.js
@@ -8,30 +8,51 @@ function addEvents() {
         if (creationInfo != "-1") {
             document.getElementById("name-input").value = creationInfo.toString().split('|')[1];
             document.getElementById("version-input").value = creationInfo.toString().split('|')[0];
-            let loader=creationInfo.toString().split('|')[2];
+            let loader = creationInfo.toString().split('|')[2];
             console.log(loader);
             console.log(loader.charAt(0).toUpperCase() + loader.slice(1));
             document.getElementById("loader-input").value = loader;
-            document.getElementById("warning").innerHTML="";
+            document.getElementById("warning").innerHTML = "";
         }
         else {
             document.getElementById("warning").innerHTML = "!Warning! The folder with the modpack that you are trying to open is most likely created not with CurseForge. For better performance and to avoid errors in Made and the modpack itself, it is recommended to recreate your modpack using CurseForge</br>"
         }
     });
     const form = document.getElementById('newProjectForm');
-    form.addEventListener('submit', async function (e){
+    form.addEventListener('submit', async function (e) {
         e.preventDefault();
         let formData = new FormData(form);
-        let pair= formData.entries();
-        let path=pair.next().value[1];
-        let name=pair.next().value[1];
-        let version=pair.next().value[1];
-        let loader=pair.next().value[1];
-        console.log(loader);
-        await invoke("add_project", { name: name, path: path,  version: version, loader:loader });
-        await invoke("set_current_project", { path:path, name:name });
-        location.href = "projectPage.html";
-      });
+        let pair = formData.entries();
+        let path = pair.next().value[1];
+        let name = pair.next().value[1];
+        let version = pair.next().value[1];
+        let loader = pair.next().value[1];
+        console.log(isVersionSupported(version,loader));
+        // if (isVersionSupported(version, loader)) {
+        //     await invoke("add_project", { name: name, path: path, version: version, loader: loader });
+        //     await invoke("set_current_project", { path: path, name: name });
+        //     location.href = "projectPage.html";
+        // }
+        // else {
+        //     showUnsupportedVersionMessage(version, loader)
+        // }
+    });
+}
+function isVersionSupported(version, loader) {
+    if (loader == "forge") {
+        if (["1.12.2", "1.14.4", "1.15.1", "1.15.2", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.16.5", "1.18", "1.18.1", "1.18.1", "1.18.2", "1.19", "1.19.2"].includes(version)) {
+            return true;
+        }
+    }
+    else if (loader == "fabric") {
+        if (["1.18.2", "1.19", "1.19.2"].includes(version)) {
+            return true;
+        }
+    }
+    return false;
+}
+function showUnsupportedVersionMessage(version) {
+
 }
 async function setThemeOnStart() {
     var linkElement = document.getElementById('theme-link');
